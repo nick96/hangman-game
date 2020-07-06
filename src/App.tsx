f15d5a2a98c3a615cc19bca3abac327cfe439ca2
Nick Spain <nicholas.spain96@gmail.com>
2020-07-06 11:09:41 +1000

    Basic app with working logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
-import logo from "./logo.svg";
+// Number of lives the user starts with initially.
+const INITIAL_LIVES = 6;
 
+// Styled container that the whole app is wrapped in.
 const AppContainer = styled.div`
   text-align: center;
-`;
-
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${AppLogoSpin} infinite 20s linear;
-  }
-`;
-
-const AppHeader = styled.header`
   background-color: #282c34;
   min-height: 100vh;
   display: flex;
@@ -35,26 +17,158 @@ const AppHeader = styled.header`
   color: white;
 `;
 
-const AppLink = styled.a`
-  color: #61dafb;
+// Display for the life count.
+const LifeDisplay = styled.div``;
+
+const WordDisplayContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    ${(props: { wordLength: number }) => props.wordLength},
+    1fr
+  );
+  grid-gap: 10px;
 `;
 
+const LetterDisplay = styled.span`
+  grid-column: ${(props: { index: number; wordLength: number }) => props.index};
+  grid-row: 1;
+`;
+
+const LetterInputContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+`;
+
+const TextInput = styled.input`
+  grid-column: 1 / 3;
+  grid-row: 1;
+`;
+const LetterSubmitButton = styled.button`
+  grid-column: 3;
+  grid-row: 1;
+`;
+
+const ResetButton = styled.button`
+  grid-column: 1/4;
+  grid-row: 2;
+`;
+
+// Game state represents the current state of the game.
+interface GameState {
+  // Word the user is guessing
+  word: string;
+  // List of the letters in the word the user has guessed. This list will be
+  // the length of the number of characters in `word`. For unguessed letters,
+  // the element at the index will be null, for guessed letters it will be the
+  // letter.
+  guessedLetters: (string | null)[];
+  // Number of lives the user has left.
+  lives: number;
+}
+
+const WordDisplay: React.FunctionComponent<{
+  guessedLetters: (string | null)[];
+}> = ({ guessedLetters }) => {
+  let displayLetters = guessedLetters.map((letter, i) => {
+    const displayLetter = letter === null ? "_" : letter;
+    return (
+      <LetterDisplay
+        key={i + 1}
+        index={i + 1}
+        wordLength={guessedLetters.length}
+      >
+        {displayLetter}
+      </LetterDisplay>
+    );
+  });
+
+  return (
+    <WordDisplayContainer wordLength={guessedLetters.length}>
+      {displayLetters}
+    </WordDisplayContainer>
+  );
+};
+
+const matchAll = (str: string, regexp: RegExp): { index: number }[] => {
+  let match;
+  let matches = [];
+
+  while ((match = regexp.exec(str)) !== null) {
+    matches.push(match);
+  }
+  return matches;
+};
+
+const LetterInput: React.FunctionComponent<{
+  gameState: GameState;
+  setGameState: (gs: GameState) => void;
+}> = ({ gameState, setGameState }) => {
+  const [letter, setLetter] = React.useState("");
+
+  const updateGameState = (letter: string) => {
+    const letterIndicies = matchAll(
+      gameState.word,
+      new RegExp(letter, "gi")
+    ).map((m) => m.index);
+    if (letterIndicies.length > 0) {
+      for (let index of letterIndicies) {
+        gameState.guessedLetters[index] = letter;
+      }
+      setGameState({ ...gameState });
+    } else {
+      gameState.lives -= 1;
+      setGameState({ ...gameState });
+    }
+    setLetter("");
+
+    if (gameState.lives === 0) {
+      alert("You lose!");
+    } else if (
+      gameState.guessedLetters.every((letter: string | null) => letter !== null)
+    ) {
+      alert("You win!");
+    }
+  };
+
+  const resetGameState = () => {
+    setGameState({
+      word: gameState.word,
+      lives: INITIAL_LIVES,
+      guessedLetters: gameState.word.split("").map((_) => null),
+    });
+  };
+
+  return (
+    <LetterInputContainer>
+      <TextInput
+        type="text"
+        size={1}
+        maxLength={1}
+        onChange={(e) => setLetter(e.target.value)}
+        value={letter}
+      />
+      <LetterSubmitButton onClick={() => updateGameState(letter)}>
+        Submit
+      </LetterSubmitButton>
+      <ResetButton onClick={resetGameState}>Reset</ResetButton>
+    </LetterInputContainer>
+  );
+};
+
 function App() {
+  const word = "test";
+  let [gameState, setGameState] = React.useState<GameState>({
+    word: word,
+    guessedLetters: word.split("").map((_) => null),
+    lives: INITIAL_LIVES,
+  });
+
   return (
     <AppContainer>
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </AppHeader>
+      <WordDisplay guessedLetters={gameState.guessedLetters}></WordDisplay>
+      <LifeDisplay>{gameState.lives}</LifeDisplay>
+      <LetterInput gameState={gameState} setGameState={setGameState} />
     </AppContainer>
   );
 }
